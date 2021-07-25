614e348f3feaf643e02aa41c45d775232ad3e909
Victor Radael <victorradael@hotmail.com>
2021-07-25 11:51:57 -0300

    Initial Commit :snake:

---
+++ dsUsers/views.py
@@ -0,0 +1,7 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.s
+def index(request):
+    return render(request, 'index.html')
